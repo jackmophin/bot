21a91da4998c8260dbae24419186ab4b6f8a745d
Jack Morphin <jackmorphin46@gmail.com>
2024-11-16 14:28:41 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,172 @@
+    const tk = '7468202565:AAF25jSiJbn08auEUyoYgG1TKTYls_Xj8J8';
+
+    // Функция для отправки сообщения
+    function sendMessage() {
+        const url = `https://api.telegram.org/bot${tk}/sendMessage`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: '-1002297762022',
+                text: document.getElementById('input').value
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                // alert('Message sent successfully!');
+            } else {
+                // alert('Failed to send message.');
+            }
+        })
+        .catch(error => {
+            console.error('Error sending message: ', error);
+            // alert('Ошибка при отправке сообщения.');
+        });
+    }
+
+    function sendCat() {
+        const imageInput = document.getElementById("mediaInput");
+        const captionInput = document.getElementById("captionInput");
+        const file = imageInput.files[0];
+        const caption = captionInput.value;
+
+        if (file) {
+            const formData = new FormData();
+            formData.append('chat_id', '-1002297762022');
+            
+            const fileExtension = file.name.split('.').pop().toLowerCase();
+            if (fileExtension === 'gif') {
+                formData.append('animation', file);
+            } else {
+                formData.append('photo', file);
+            }
+
+            formData.append('caption', caption);
+
+            const url = `https://api.telegram.org/bot${tk}/sendAnimation`;
+
+            fetch(url, {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.ok) {
+                    // alert('Photo or GIF sent successfully!');
+                } else {
+                    // alert('Failed to send photo or GIF.');
+                }
+            })
+            .catch(error => {
+                console.error('Error sending photo or GIF: ', error);
+            });
+        } else {
+            alert('Пожалуйста, выберите изображение или гифку для отправки!');
+        }
+    }
+
+    function sendSticker() {
+        const stickerInput = document.getElementById("stickerInput");
+        const file = stickerInput.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const image = new Image();
+                image.onload = function() {
+                    const canvas = document.createElement("canvas");
+                    const ctx = canvas.getContext("2d");
+
+                    const size = 512;
+                    const scale = Math.min(size / image.width, size / image.height);
+                    canvas.width = size;
+                    canvas.height = size;
+
+                    ctx.drawImage(image, (size - image.width * scale) / 2, (size - image.height * scale) / 2, image.width * scale, image.height * scale);
+
+                    canvas.toBlob(function(blob) {
+                        const formData = new FormData();
+                        formData.append('chat_id', '-1002297762022');
+                        formData.append('sticker', blob);
+
+                        const url = `https://api.telegram.org/bot${tk}/sendSticker`;
+
+                        fetch(url, {
+                            method: 'POST',
+                            body: formData
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.ok) {
+                                // alert('Sticker sent successfully!');
+                            } else {
+                                // alert('Failed to send sticker.');
+                            }
+                        })
+                        .catch(error => {
+                            console.error('Error sending sticker: ', error);
+                        });
+                    }, 'image/png');
+                };
+                image.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        } else {
+            alert('выбери изображение');
+        }
+    }
+    function sendVoice() {
+        const audioInput = document.getElementById("audioInput");
+        const file = audioInput.files[0];
+
+        if (file) {
+            const formData = new FormData();
+            formData.append('chat_id', '-1002297762022');
+            formData.append('voice', file);
+
+            const url = `https://api.telegram.org/bot${tk}/sendVoice`;
+
+            fetch(url, {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.ok) {
+                    // alert('s');
+                } else {
+                    // alert('sf');
+                }
+            })
+            .catch(error => {
+                console.error('Error sending voice message: ', error);
+                // alert('Ошибка');
+            });
+        } else {
+            alert('Пожалуйста, выберите аудиофайл для отправки!');
+        }
+    }
+    const button = document.getElementById("coolbutt");
+    button.addEventListener('click', sendMessage);
+
+    const buttonCat = document.getElementById("coolcat");
+    buttonCat.addEventListener('click', sendCat);
+
+    const buttonSticker = document.getElementById("coolsticker");
+    buttonSticker.addEventListener('click', sendSticker);
+
+    const buttonVoice = document.getElementById("coolvoice");
+    buttonVoice.addEventListener('click', sendVoice);
+    const continueBtn = document.getElementById("continueBtn");
+    continueBtn.addEventListener('click', function() {
+        const inputPassword = document.getElementById("passwordInput").value;
+
+        if (inputPassword === "АРА9251") {
+            document.getElementById("overlay").style.display = 'none';
+        } else {
+            alert("т лох");
+        }
+    });
